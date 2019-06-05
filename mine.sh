e018d4c6fac9d00074136f64c6f0743fda19d83b
perx <jaspreet.hd@gmail.com>
2019-06-05 00:33:32 +0000

    fetched change commits from bugsjson

--- mine.sh
+++ mine.sh
@@ -2,4 +2,4 @@
 # Builds a database of <commit,basic change type> pairs by mining git repos
 # Output: a .csv file containing the relations
 
-mvn exec:java -X -Dexec.mainClass="ca.ubc.ece.salt.pangor.js.learn.LearningAnalysisMain" -Dexec.args="--repositories ./input/javascript_repositories.txt --dataset ./output/dataset.csv --regex \"fix|repair|bug|error|resolve|close|issue\" --threads 1"
+mvn exec:java -X -Dexec.mainClass="ca.ubc.ece.salt.pangor.js.learn.LearningAnalysisMain" -Dexec.args="--repositories ./input/test.txt --dataset ./output/test.csv --regex \"fix|repair|bug|error|resolve|close|issue\" --threads 4"
