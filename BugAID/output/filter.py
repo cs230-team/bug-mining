0cde069b9c84dd6fe59ad7e013b606e9c7dd59ec
perx <jaspreet.hd@gmail.com>
2019-06-07 11:31:23 +0000

    added commit ids from bugsjs

--- BugAID/output/filter.py
+++ BugAID/output/filter.py
@@ -1,4 +1,5 @@
 import os
+import numpy as np
 
 #f1 = open('test.csv','rb')
 def find_next(a,start):
@@ -6,18 +7,19 @@ def find_next(a,start):
 	if len(s)==0:
 		return 0
 	i=0
-	while s[i]!=',':
+	while s[i]!=',' and i<len(s):
 		i=i+1
-	return i+1
+	return i+start
 
-f1 = open('bugsjsonall.csv', 'rb')
+f1 = open('bugsjsonrepos.csv', 'rb')
 f2 = open('bugsjson.csv','w')
+ids = np.load('commits.npy')
 bugs = f1.readlines()
 index=1
 for bug in bugs:
 	bugid = find_next(bug,0)
-	commit = find_next(bug,find_next(bug,find_next(bug,bugid)))
-	if True:
+	commit = find_next(bug,find_next(bug,find_next(bug,find_next(bug,bugid))))
+	if commit in ids:
 		f2.write('{},'.format(index)+bug[bugid:])
 		index+=1
 #f1.close()
