913ff6deda7b022509b829feb27c8ddc130f1ac3
Anurag Pant <pant_anurag555@yahoo.com>
2019-06-08 15:45:01 -0700

    Check to include only defects4j patterns.

--- Bug Cluster-Class Comparison/compare_bug_clusters_classes_fixminer.py
+++ Bug Cluster-Class Comparison/compare_bug_clusters_classes_fixminer.py
@@ -14,6 +14,14 @@ def load_zipped_pickle(filename):
 
 clusters = load_zipped_pickle('defects4jcluster.pickle')
 
+with open('classification.json','r',encoding='utf8') as read_file:
+        classification = json.load(read_file)
+
+classification_types = set()
+for category in classification['Repair Patterns']:
+        for pattern in classification['Repair Patterns'][category]:
+                classification_types.add(pattern)
+
 bug_patterns = {}
 
 for bug in d4_bugs:
@@ -31,15 +39,24 @@ for category in categories:
                 cluster_num = int(subset.loc[i]['cid'][0][idx+1:])
                 for bug in subset.loc[i]['defects4j']:
                         if cluster_num in category_dict:
+                                patterns_before = category_dict[cluster_num]['pattern_count']
                                 for pattern in bug_patterns[bug]:
-                                        if pattern in category_dict[cluster_num]:
-                                                category_dict[cluster_num][pattern] += 1
-                                        else:
-                                                category_dict[cluster_num][pattern] = 1
-                                category_dict[cluster_num]['bug_count'] += 1
+                                        if pattern in classification_types:
+                                                category_dict[cluster_num]['pattern_count'] += 1
+                                                if pattern in category_dict[cluster_num]:
+                                                        category_dict[cluster_num][pattern] += 1
+                                                else:
+                                                        category_dict[cluster_num][pattern] = 1
+                                if category_dict[cluster_num]['pattern_count'] != patterns_before:
+                                        category_dict[cluster_num]['bug_count'] += 1
                         else:
                                 category_dict[cluster_num] = {}
+                                category_dict[cluster_num]['pattern_count'] = 0
+                                patterns_before = category_dict[cluster_num]['pattern_count']
                                 for pattern in bug_patterns[bug]:
-                                        category_dict[cluster_num][pattern] = 1
-                                category_dict[cluster_num]['bug_count'] = 1
+                                        if pattern in classification_types:
+                                                category_dict[cluster_num]['pattern_count'] += 1
+                                                category_dict[cluster_num][pattern] = 1
+                                if category_dict[cluster_num]['pattern_count'] != patterns_before:
+                                        category_dict[cluster_num]['bug_count'] = 1
         categories_dict[category] = category_dict
