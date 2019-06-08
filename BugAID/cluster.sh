5262d9968e0bdc4fc1300fe73b1495ec7b54a700
perx <jaspreet.hd@gmail.com>
2019-06-08 02:45:58 +0000

    generated reduced clusters

--- BugAID/cluster.sh
+++ BugAID/cluster.sh
@@ -2,4 +2,4 @@
 # Generates change types by clustering a dataset
 # Output: an .arff file containing the clusters
 
-mvn exec:java -X -Dexec.mainClass="ca.ubc.ece.salt.pangor.learn.LearningDataSetMain" -Dexec.args="--dataset ./output/bugsjsonall.csv --complexity 6 --epsilon 0.3 --complexityWeight 0.2 --minClusterSize 5"
+mvn exec:java -X -Dexec.mainClass="ca.ubc.ece.salt.pangor.learn.LearningDataSetMain" -Dexec.args="--dataset ./output/bugsjson.csv --complexity 6 --epsilon 0.3 --complexityWeight 0.2 --minClusterSize 1"
--- BugAID/output/filter.py
+++ BugAID/output/filter.py
@@ -3,24 +3,27 @@ import numpy as np
 
 #f1 = open('test.csv','rb')
 def find_next(a,start):
-	s=a[start:]
+	s=a[start+1:]
+	#print(s)
 	if len(s)==0:
 		return 0
 	i=0
 	while s[i]!=',' and i<len(s):
+	#	print(i)
 		i=i+1
-	return i+start
+	return i+start+1
 
-f1 = open('bugsjsonrepos.csv', 'rb')
+f1 = open('bugsjsonall.csv', 'r')
 f2 = open('bugsjson.csv','w')
 ids = np.load('commits.npy')
 bugs = f1.readlines()
 index=1
 for bug in bugs:
-	bugid = find_next(bug,0)
-	commit = find_next(bug,find_next(bug,find_next(bug,find_next(bug,bugid))))
-	if commit in ids:
-		f2.write('{},'.format(index)+bug[bugid:])
+	bug_arr=bug.split(",")
+	commit=bug_arr[4]
+	if commit in ids or bug_arr[5] in ids:
+		bugid=find_next(bug,0)
+		f2.write('{}'.format(index)+bug[bugid:])
 		index+=1
 #f1.close()
 f2.close()
