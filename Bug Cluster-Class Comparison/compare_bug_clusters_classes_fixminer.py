71c06124f257467410fb8460f86fb37c39dbd640
Anurag Pant <pant_anurag555@yahoo.com>
2019-06-08 05:31:54 -0700

    Created dictionary of patterns in all clusters.

--- Bug Cluster-Class Comparison/compare_bug_clusters_classes_fixminer.py
+++ Bug Cluster-Class Comparison/compare_bug_clusters_classes_fixminer.py
@@ -12,9 +12,34 @@ def load_zipped_pickle(filename):
         loaded_object = pickle.load(f)
         return loaded_object
 
-cluster = load_zipped_pickle('defects4jcluster.pickle')
+clusters = load_zipped_pickle('defects4jcluster.pickle')
 
 bug_patterns = {}
 
 for bug in d4_bugs:
         bug_patterns[bug['program'] + '-' + str(bug['bugId'])] = bug['repairPatterns']
+
+categories = ['tokens','shapes','actions']
+categories_dict = {}
+
+for category in categories:
+        category_dict = {}
+        subset = clusters[clusters['type'] == category]
+        subset = subset.reset_index()
+        for i in range(len(subset)):
+                idx = subset.loc[i]['cid'][0].rfind('-')
+                cluster_num = int(subset.loc[i]['cid'][0][idx+1:])
+                for bug in subset.loc[i]['defects4j']:
+                        if cluster_num in category_dict:
+                                for pattern in bug_patterns[bug]:
+                                        if pattern in category_dict[cluster_num]:
+                                                category_dict[cluster_num][pattern] += 1
+                                        else:
+                                                category_dict[cluster_num][pattern] = 1
+                                category_dict[cluster_num]['bug_count'] += 1
+                        else:
+                                category_dict[cluster_num] = {}
+                                for pattern in bug_patterns[bug]:
+                                        category_dict[cluster_num][pattern] = 1
+                                category_dict[cluster_num]['bug_count'] = 1
+        categories_dict[category] = category_dict
