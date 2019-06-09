6d32846f47dde0b80ac0911e924eaee6efc1ea8a
Anurag Pant <pant_anurag555@yahoo.com>
2019-06-08 23:55:00 -0700

    Visualize significant patterns in clusters

--- Bug Cluster-Class Comparison/compare_bug_clusters_classes_fixminer.py
+++ Bug Cluster-Class Comparison/compare_bug_clusters_classes_fixminer.py
@@ -61,17 +61,34 @@ for category in categories:
                                         category_dict[cluster_num]['bug_count'] = 1
         categories_dict[category] = category_dict
 
+membership = {}
+
 # calculate pattern membership in each cluster based on number of bugs detected
 for tree_type in categories_dict:
-        print('Tree',tree_type)
+        cluster_patterns = {}
+        # print('Tree',tree_type)
         for cluster_num in categories_dict[tree_type]:
                 if categories_dict[tree_type][cluster_num]['bug_count'] > 1:
-                        print('')
-                        print('\tCluster Number',cluster_num)
+                        # print('')
+                        # print('\tCluster Number',cluster_num)
                         for pattern in categories_dict[tree_type][cluster_num]:
                                 if pattern != 'pattern_count' and pattern != 'bug_count':
                                         tmp = categories_dict[tree_type][cluster_num][pattern] / categories_dict[tree_type][cluster_num]['bug_count']
                                         if tmp >= 0.3:
-                                                print('\t\t',pattern,'with confidence',tmp)
+                                                # print('\t\t',pattern,'with confidence',tmp)
+                                                if pattern in cluster_patterns:
+                                                        if tmp > cluster_patterns[pattern][1]:
+                                                                cluster_patterns[pattern] = (cluster_num,tmp)
+                                                else:
+                                                        cluster_patterns[pattern] = (cluster_num,tmp)
+        membership[tree_type] = cluster_patterns
+        # print('')
+        # print('')
+
+for tree_type in membership:
+        print('Tree',tree_type)
+        for pattern in membership[tree_type]:
+                print('')
+                print('\tCluster Number',membership[tree_type][pattern][0],'Pattern',pattern,'Confidence',membership[tree_type][pattern][1])
         print('')
         print('')
